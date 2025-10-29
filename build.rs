cb5adba13fcaf10336981330535cecba8eb184e3
bpronin <boris.i.pronin@gmail.com>
2025-10-29 20:19:12 +0300

    add elevation request

---
+++ build.rs
@@ -0,0 +1,13 @@
+﻿use embed_manifest::manifest::ExecutionLevel;
+use embed_manifest::{embed_manifest, new_manifest};
+
+fn main() {
+    if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        embed_manifest(
+            new_manifest("app.manifest")
+                .requested_execution_level(ExecutionLevel::RequireAdministrator),
+        )
+        .expect("unable to embed manifest file");
+    }
+    println!("cargo:rerun-if-changed=build.rs");
+}
